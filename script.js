83d349094e9572fd9b3396c565b8fdcc8c3bf874
Omkar Joshi <joshiomkar0400@gmail.com>
2021-01-20 16:33:30 +0530

    changes in delete button

--- script.js
+++ script.js
@@ -37,7 +37,11 @@ function deleteCheck(e){
     if(item.classList[0] === "delete-btn"){
        
         const todo = item.parentElement;
-        todo.remove();
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend",function(){
+            todo.remove();
+        })
+        
     }
 
     if(item.classList[0] === "complete-btn"){
